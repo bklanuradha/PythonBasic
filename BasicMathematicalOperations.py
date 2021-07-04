79cd3ae9534f37aa9300ce208c67e55baf33cb3f
bklanuradha <92729@fhss.sjp.ac.lk>
2021-07-04 06:48:33 +0000

    MathematicalOperations

---
+++ BasicMathematicalOperations.py
@@ -0,0 +1,37 @@
+# addition
+a = 10
+b = 15
+c = (a + b)
+print(c)
+
+# Subtraction
+a = 14
+b = -11
+c = (a - b)
+print(c)
+
+# multiplication
+a = 6
+b = 5
+c = (a * b)
+print(c)
+
+# Division
+a = 81
+b = 9
+c =(a / b)
+print(c)
+
+# Modulo Operator
+a = 20
+b = 8
+c = (a % b)
+print(c)
+
+# Power Operator
+a = 10
+b = 3
+c = (a ** b)
+print(c)
+
+
