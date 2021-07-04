fefb9e00ea24d584deead2dc16d61aed7a7afd90
bklanuradha <92729@fhss.sjp.ac.lk>
2021-07-04 07:39:14 +0000

    MultiplyUserInputs

---
+++ MultiplyUserInput.py
@@ -0,0 +1,9 @@
+# getting two numbers user input
+a = int(input("Enter Number: "))
+b = int(input("Enter Number: "))
+
+# Multiply Two Number
+c = (a * b)
+
+# Print Value
+print("Finale value: ", c)
