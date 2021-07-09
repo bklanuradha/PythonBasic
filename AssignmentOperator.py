4636fb233400eba360117ea5553c455d8003d8c8
bklanuradha <92729@fhss.sjp.ac.lk>
2021-07-09 13:01:32 +0000

    AssignmentOperator.py

---
+++ AssignmentOperator.py
@@ -0,0 +1,26 @@
+a = 20
+b = 40
+
+# b = b + a
+# b += a
+
+# b = b - a
+# b -= a
+
+# b = b * a
+# b *= a
+
+# b = b / a
+# b /= a
+
+# b = b % a
+# b %= a
+
+# b = b ** a
+# b **= a
+
+# b = b // a
+# b //= a
+
+
+print(b)
