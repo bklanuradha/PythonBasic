8a5e2da935a67f690b738cf6a6e1598968f6dc63
bklanuradha <92729@fhss.sjp.ac.lk>
2021-07-09 12:20:35 +0000

    ComparisonOperator.py

---
+++ ComparisonOperator.py
@@ -0,0 +1,28 @@
+# getting two number
+a = 15
+b = 30
+
+# equal operator
+print(a == b)
+print(a == 15)
+
+# Not equal operator
+print(a != b)
+print(a != 15)
+
+# Grater than operator
+print(a > b)
+print(a > 15)
+
+# less than operator
+print(a < b)
+print(a < 15)
+
+# Grater than or equal operator
+print(a >= b)
+print(a >= 15)
+
+# Less than or equal operator
+print(a <= b)
+print(a <= 15)
+
