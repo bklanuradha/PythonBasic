e170bd03e326fd01a7d7b5f0c5dcddec58278eea
bklanuradha <92729@fhss.sjp.ac.lk>
2021-07-10 03:58:15 +0000

    Import.py

---
+++ Import.py
@@ -0,0 +1,8 @@
+import input2Num as x
+
+# calculation
+answer = x.num1 + x.num2
+
+# Output
+print("Answer: ", answer)
+
---
+++ input2Num.py
@@ -0,0 +1,3 @@
+# getting user Inputs
+num1 = float(input("Input Number 01: "))
+num2 = float(input("Input Number 02: "))
