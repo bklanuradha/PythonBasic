44f6a9d8699296778953aca2646efd49f9115f2b
bklanuradha <92729@fhss.sjp.ac.lk>
2021-07-12 15:22:00 +0000

    findReplace.py

---
+++ findReplace.py
@@ -0,0 +1,26 @@
+# string = "The surprise is here somewhere"
+# num = "45-455-44555"
+
+# x = string.find("surprise")
+# y = num.find("5")
+
+# print(x)
+# print(y)
+
+# print("The surprise is here somewhere".find("ittdfg"))
+# print("The surprise is here somewhere".find("is "))
+
+# ###########################################
+
+replaceString = "The surprise is here somewhere"
+print(replaceString)
+replaceString = replaceString.replace("is ", "isn't ")
+print(replaceString)
+
+replaceNum = "45-455-44555"
+print(replaceNum)
+replaceNum = replaceNum[0:2].replace("5", "7")
+print(replaceNum)
+
+
+
